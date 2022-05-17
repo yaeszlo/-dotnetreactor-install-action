c40db5b24939ae0fb318de1ec78168bfb897a7c3
yaeszlo <laszlo@cryptonovae.com>
2022-05-17 18:38:23 +0200

    Update script paths

--- main.js
+++ main.js
@@ -2,26 +2,26 @@ const {existsSync, writeFileSync} = require('fs');
 const {execSync} = require('child_process');
 const {getInput} = require('@actions/core')
 
-const licenseFilePath = "C:\\Program Files (x86)\\Eziriz\\.NET Reactor\\license.v3lic"
-const dotreactorPath = "C:\\Program Files (x86)\\Eziriz\\.NET Reactor\\dotNET_Reactor.Console.exe"
+const licenseFilePath = "Eziriz\\.NET Reactor\\license.v3lic"
+const dotreactorPath = "Eziriz\\.NET Reactor\\dotNET_Reactor.Console.exe"
 
 function installDotreactor() {
   console.log('[Install .NET Reactor]')
   if (!existsSync(dotreactorPath)){
-    console.log('- Installing...')
-    execSync('START /WAIT dnr_setup_latest_build.exe /silent');
-    console.log('- Done.')
+    console.log('- Installing...');
+    execSync(`"START /WAIT dnr_setup_latest_build.exe /silent /DIR=.\\"`);
+    console.log('- Done.');
   } else {
-    console.log('- Already installed.')
+    console.log('- Already installed.');
   }
 }
 
 function writeLicenseFile() {
-  console.log('[Set up license]')
+  console.log('[Set up license]');
   const licenseData = decodeBase64String(getInput('license'));
-  console.log('- Writing license file...')
+  console.log('- Writing license file...');
   writeFileSync(licenseFilePath, licenseData);
-  console.log('- Done.')
+  console.log('- Done.');
 }
 
 function decodeBase64String(string) {
