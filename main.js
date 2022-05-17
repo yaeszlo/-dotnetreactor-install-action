055b291e5a1bdfb80853266438f2e97cdff1ae55
yaeszlo <laszlo@cryptonovae.com>
2022-05-17 10:07:08 +0200

    Update license file path

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ const {execSync} = require('child_process');
 const {getInput} = require('@actions/core')
 const {writeFileSync} = require('fs');
 
-const licenseFilePath = './license.v3lic'
+const licenseFilePath = "C:\\Program Files (x86)\\Eziriz\\.NET Reactor\\license.v3lic"
+const dotreactorPath = "C:\\Program Files (x86)\\Eziriz\\.NET Reactor\\dotNET_Reactor.Console.exe"
 
-function installDotreactor(){
+function installDotreactor() {
   execSync('START /WAIT dnr_setup_latest_build.exe /silent');
 }
 
-function writeLicenseFile(){
+function writeLicenseFile() {
   const licenseData = decodeBase64String(getInput('license'));
   writeFileSync(licenseFilePath, licenseData);
 }
@@ -17,12 +18,12 @@ function decodeBase64String(string) {
   return Buffer.from(string, "base64").toString("utf-8");
 }
 
-function obfuscateFiles(){
-  const projectPath = getInput('project_path')
-  execSync(`"C:\\Program Files (x86)\\Eziriz\\.NET Reactor\\dotNET_Reactor.Console.exe" -project "${projectPath}"`)
+function obfuscateFiles() {
+  const projectPath = getInput('project_path');
+  execSync(`"${dotreactorPath}" -project "${projectPath}"`);
 }
 
-exports.default = function() {
+exports.default = function () {
   installDotreactor();
   writeLicenseFile();
   obfuscateFiles();
