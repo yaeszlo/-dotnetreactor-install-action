ce4f0dbde0858bd570508a3d165228811054894e
yaeszlo <laszlo@cryptonovae.com>
2022-05-17 09:10:20 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const {execSync} = require('child_process');
+const {getInput} = require('@actions/core')
+const {writeFileSync} = require('fs');
+
+const licenseFilePath = './license.v3lic'
+
+function installDotreactor(){
+  execSync('START /WAIT dnr_setup_latest_build.exe /silent');
+}
+
+function writeLicenseFile(){
+  const licenseData = decodeBase64String(getInput('license'));
+  writeFileSync(licenseFilePath, licenseData);
+}
+
+function decodeBase64String(string) {
+  return Buffer.from(string, "base64").toString("utf-8");
+}
+
+function obfuscateFiles(){
+  const projectPath = getInput('project_path')
+  execSync(`"C:\\Program Files (x86)\\Eziriz\\.NET Reactor\\dotNET_Reactor.Console.exe" -project "${projectPath}"`)
+}
+
+exports.default = function() {
+  installDotreactor();
+  writeLicenseFile();
+  obfuscateFiles();
+}
